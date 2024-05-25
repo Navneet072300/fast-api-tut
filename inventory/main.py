ceeeb13554a98606e51aeafe6328cc5596d5f1c4
Navneet <navneetshahi345@gmail.com>
2024-05-26 00:07:30 +0530

    "Added CORS middleware, replaced Redis connection details, removed root endpoint, and added CRUD endpoints for Product model using Redis OM"

--- inventory/main.py
+++ inventory/main.py
@@ -1,16 +1,59 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 redis = get_redis_connection(
-    host="redis-14012.c15.us-east-1-4.ec2.redns.redis-cloud.com",
+    host="redis-11844.c135.eu-central-1-1.ec2.cloud.redislabs.com",
     port=11844,
-    password="kQvN2PVgKy20xteMFwYXPVVEKNapY9rk",
+    password="pRdcpRkKPFn6UnEFskrDGxrmFbf5T9ER",
     decode_responses=True
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
