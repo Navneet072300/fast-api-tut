26a76c8793c495841315a73e3fcc62f1ce7f0330
Navneet <navneetshahi345@gmail.com>
2024-05-25 23:43:04 +0530

    "Added Redis connection to main.py and updated compiled main.cpython-310.pyc file"

--- inventory/main.py
+++ inventory/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection
 
 app = FastAPI()
+
+redis = get_redis_connection(
+    host="redis-14012.c15.us-east-1-4.ec2.redns.redis-cloud.com",
+    port=11844,
+    password="kQvN2PVgKy20xteMFwYXPVVEKNapY9rk",
+    decode_responses=True
+)
 
 
 @app.get("/")
